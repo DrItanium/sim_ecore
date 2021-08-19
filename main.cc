ae0f26944e761d6d8d74402a4d7540d1af4f1de8
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-08-19 15:03:06 -0700

    Fixup i960ZCT to be based off of the i960SB core

--- main.cc
+++ main.cc
@@ -20,92 +20,23 @@
 // ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#include "SimplifiedSxCore.h"
-#include <array>
-#include <iostream>
-#include <memory>
-union MemoryCell {
-    constexpr MemoryCell(Ordinal value = 0) noexcept : raw(value) { }
-
-    Ordinal raw;
-#define X(type, name) type name [ sizeof(raw) / sizeof(type)]
-    X(ShortOrdinal, ordinalShorts);
-    X(ShortInteger, integerShorts);
-    X(ByteOrdinal, ordinalBytes);
-    X(ByteInteger, integerBytes);
-#undef X
-};
-static_assert(sizeof(MemoryCell) == sizeof(Ordinal), "MemoryCell must be the same size as a long ordinal");
+#include "SBCore.h"
 /**
  * @brief A theoretical i960Sx derived core with 64 megabytes of built in ram.
  * Everything runs from ram in this implementation
  */
-class ZCT_Core : public SimplifiedSxCore {
+class ZCT_Core : public SBCore {
 public:
-    using Parent = SimplifiedSxCore;
+    using Parent = SBCore;
     using Parent::Parent;
     static constexpr Address HaltRegisterOffset = 0x00FF'FFFC;
     static constexpr Address ConsoleRegisterOffset = 0x00E0'0000;
     static constexpr Address ConsoleFlushOffset = 0x00E0'0004;
-    static constexpr Address IACBaseAddress = 0x0000'0010;
     static constexpr size_t MemorySize = 64_MB / sizeof(MemoryCell);
-    ZCT_Core() : Parent(), memory_(std::make_unique<MemoryCell[]>(MemorySize)) {}
     ~ZCT_Core() override = default;
-    void clearMemory() noexcept {
-        for (size_t i = 0; i < MemorySize; ++i) {
-            memory_[i].raw = 0;
-        }
-    }
-    /**
-     * @brief Install an ordinal to a given memory address
-     * @param loc
-     * @param value
-     */
-    void installToMemory(Address loc, Ordinal value) {
-        auto alignedAddress = ((64_MB -1) & loc) >> 2;
-        memory_[alignedAddress].raw = value;
-    }
-    template<typename ... Rest>
-    void installBlockToMemory(Address base, Ordinal curr, Rest&& ... values) noexcept {
-        installToMemory(base, curr);
-        installBlockToMemory(base + 4, values...);
-    }
-    void installBlockToMemory(Address base, Ordinal curr) noexcept  {
-        installToMemory(base, curr);
-    }
 protected:
     Ordinal load(Address address) override {
-        // get target thing
-        if (inRAMArea(address)) {
-            auto alignedAddress = address >> 2;
-            auto offset = address & 0b11;
-            auto& cell = memory_[alignedAddress];
-            switch (offset) {
-                case 0b00: // ah... aligned :D
-                    return cell.raw;
-                case 0b01: // upper 24 bits of current cell + lowest 8 bits of next cell
-                    return [this, &cell, alignedAddress]() {
-                        auto cell2 = static_cast<Ordinal>(memory_[alignedAddress+1].ordinalBytes[0]) << 24;
-                        auto lowerPart = cell.raw >> 8;
-                        return cell2 | lowerPart;
-                    }();
-                case 0b10: // lower is in this cell, upper is in the next cell
-                    return [this, &cell, alignedAddress]() {
-                        auto upperPart = static_cast<Ordinal>(memory_[alignedAddress+ 1].ordinalShorts[0]) << 16; // instant alignment
-                        auto lowerPart = static_cast<Ordinal>(cell.ordinalShorts[1]);
-                        return upperPart | lowerPart;
-                    }();
-                case 0b11: // requires reading a second word... gross
-                    return [this, &cell, alignedAddress]() {
-                        auto cell2 = (memory_[alignedAddress+ 1].raw) << 8; // instant alignment
-                        auto lowerPart = static_cast<Ordinal>(cell.ordinalBytes[0b11]);
-                        return cell2 | lowerPart;
-                    }();
-                default:
-                    throw "IMPOSSIBLE PATH";
-            }
-        } else if (inIACSpace(address)) {
-            // we use IAC space as a hack to "map" in all of our peripherals for this custom core
+        if (inIACSpace(address)) {
             switch (address & 0x00FF'FFFF) {
                 case HaltRegisterOffset:
                     haltExecution();
@@ -116,46 +47,16 @@ protected:
                     std::cout.flush();
                     break;
                 default:
-                    return 0;
+                    break;
             }
+            return 0;
+        } else {
+            return Parent::load(address);
         }
-        return 0;
     }
-
     void store(Address address, Ordinal value) override {
-        if (inRAMArea(address)) {
-            auto alignedAddress = address >> 2;
-            auto offset = address & 0b11;
-            auto& cell = memory_[alignedAddress];
-            MemoryCell temp(value);
-            switch (offset) {
-                case 0b00: // ah... aligned :D
-                    cell.raw = value;
-                    break;
-                case 0b01: // upper 24 bits of current cell + lowest 8 bits of next cell
-                    memory_[address + 3].ordinalBytes[0] = temp.ordinalBytes[3];
-                    cell.ordinalBytes[3] = temp.ordinalBytes[2];
-                    cell.ordinalBytes[2] = temp.ordinalBytes[1];
-                    cell.ordinalBytes[1] = temp.ordinalBytes[0];
-                    // we have to store the lower 24 bits into memory
-                    break;
-                case 0b10: // lower is in this cell, upper is in the next cell
-                    cell.ordinalShorts[1] = temp.ordinalShorts[0];
-                    memory_[address + 3].ordinalShorts[0] = temp.ordinalShorts[1];
-                    break;
-                case 0b11: // lower 24-bits next word, upper 8 bits, this word
-                    [this, &cell, address, &temp]() {
-                        auto& cell2 = memory_[address + 1];
-                        cell2.ordinalBytes[2] = temp.ordinalBytes[3];
-                        cell2.ordinalBytes[1] = temp.ordinalBytes[2];
-                        cell2.ordinalBytes[0] = temp.ordinalBytes[1];
-                        cell.ordinalBytes[3] = temp.ordinalBytes[0];
-                    }();
-                    break;
-                default:
-                    throw "IMPOSSIBLE PATH";
-            }
-        } else if (inIACSpace(address)) {
+        if (inIACSpace(address))  {
+            // first check and see if it is in the processor mapped area
             switch (address & 0x00FF'FFFF) {
                 case HaltRegisterOffset:
                     haltExecution();
@@ -171,8 +72,11 @@ protected:
                     // do nothing
                     break;
             }
+        } else {
+            Parent::store(address, value);
         }
     }
+
     void generateFault(FaultType ) override {
         std::cout << "FAULT GENERATED! HALTING!" << std::endl;
         haltExecution();
