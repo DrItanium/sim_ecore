d98d6cf4ab4f1c8256f7d1c43b1e5c7593051921
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-08-28 17:57:16 -0700

    Make psram speeds configurable

--- include/PSRAMChip.h
+++ include/PSRAMChip.h
@@ -8,11 +8,15 @@
 #include <SPI.h>
 #include "MemoryThing.h"
 
-template<int pin>
+template<int pin, uint64_t MaxClockSpeed = 5_MHz>
 class PSRAMChip : public MemoryThing
 {
 public:
     static constexpr auto EnablePin = pin;
+    static SPISettings& getSettings() noexcept {
+        static SPISettings settings(MaxClockSpeed, MSBFIRST, SPI_MODE0);
+        return settings;
+    }
 public:
     explicit PSRAMChip(Address baseAddress) noexcept : MemoryThing(baseAddress, baseAddress + 8_MB) { }
     ~PSRAMChip() override = default;
@@ -21,15 +25,18 @@ public:
         digitalWrite(EnablePin, HIGH);
         SPI.begin();
         delayMicroseconds(200); // give the psram chip time to come up
+        SPI.beginTransaction(getSettings());
         digitalWrite(EnablePin, LOW);
         SPI.transfer(0x66);
         digitalWrite(EnablePin, HIGH);
         digitalWrite(EnablePin, LOW);
         SPI.transfer(0x99);
         digitalWrite(EnablePin, HIGH);
+        SPI.endTransaction();
     }
 protected:
     size_t blockWrite(Address baseAddress, uint8_t *buf, size_t amount) noexcept override {
+        SPI.beginTransaction(getSettings());
         digitalWrite(EnablePin, LOW);
         SPI.transfer(0x02);
         SPI.transfer(baseAddress >> 16);
@@ -37,9 +44,11 @@ protected:
         SPI.transfer(baseAddress);
         SPI.transfer(buf, amount);
         digitalWrite(EnablePin, HIGH);
+        SPI.endTransaction();
         return amount;
     }
     size_t blockRead(Address baseAddress, uint8_t *buf, size_t amount) noexcept override {
+        SPI.beginTransaction(getSettings());
         digitalWrite(EnablePin, LOW);
         SPI.transfer(0x03);
         SPI.transfer(baseAddress >> 16);
@@ -47,6 +56,7 @@ protected:
         SPI.transfer(baseAddress);
         SPI.transfer(buf, amount);
         digitalWrite(EnablePin, HIGH);
+        SPI.endTransaction();
         return amount;
     }
 };
