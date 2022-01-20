fdb2e7f168e38bde50676a77eeabf933a4925558
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-01-20 15:29:59 -0800

    Compact calling the test instructions as well

--- src/Core.cc
+++ src/Core.cc
@@ -358,29 +358,16 @@ Core::executeInstruction(const Instruction &instruction) noexcept {
             condFault((static_cast<uint8_t>(theOpcode) >> 4) & 0b111);
             break;
             // COBR Format
+            // just like with fault and branch, test has the mask embedded in the opcode, so extract it out
         case Opcode::testno:
-            testOp(0b000);
-            break;
         case Opcode::testg:
-            testOp(0b001);
-            break;
         case Opcode::teste:
-            testOp(0b010);
-            break;
         case Opcode::testge:
-            testOp(0b011);
-            break;
         case Opcode::testl:
-            testOp(0b100);
-            break;
         case Opcode::testne:
-            testOp(0b101);
-            break;
         case Opcode::testle:
-            testOp(0b110);
-            break;
         case Opcode::testo:
-            testOp(0b111);
+            testOp((static_cast<uint8_t>(theOpcode) >> 4) & 0b111);
             break;
         case Opcode::bbc:
             // branch if bit is clear
