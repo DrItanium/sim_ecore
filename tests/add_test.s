4ee0f7f54f206cca1db8c32b34e0d5b20dc26f46
Joshua Scoggins <Theoretically.x64@gmail.com>
2022-01-22 18:33:36 -0800

    SanityCheckProgram has more routines

--- tests/add_test.s
+++ tests/add_test.s
@@ -1,18 +1,44 @@
 .text 
+addo_test:
 	ldconst 1, g1
 	ldconst 2, g0 
 	addo g1, g0, g2
 	cmpo g2, 3
 	faultne
+
+subo_test:
+	ldconst 1, g1
+	ldconst 2, g0 
 	subo g1, g0, g2
 	cmpo g2, 1
 	faultne
+
+mulo_test:
+	ldconst 3, g1
+	ldconst 2, g0 
+	mulo g1, g0, g2
+	cmpo g2, 6
+	faultne
+
+addi_test:
 	ldconst -3, g0
 	ldconst -4, g1
 	ldconst -7, g2
 	addi g1, g0, g3
 	cmpi g3, g2
 	faultne 
+
+subi_test:
+	ldconst -3, g0
+	ldconst -4, g1
 	subi g1, g0, g2
 	cmpi g3, 1
 	faultne
+
+muli_test:
+	ldconst -3, g0
+	ldconst -4, g1
+	muli g1, g0, g2
+	cmpi g3, 12
+	faultne
+
